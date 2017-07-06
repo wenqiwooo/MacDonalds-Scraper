de1ca4723dfa72b1c50a4e964fa906a1b0a83ee3
therealwenqi <therealwenqi@hotmail.com>
2017-07-06 06:46:45 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# MacDonald Parser 
+
+
+## Environment Setup
+You will need to use PIP to install the dependencies first.
+`pip install -r requirements.txt`
+
+Go to http://phantomjs.org/download.html and download latest the version of phantomjs for your OS.
+
+Update `PHANTOMJS_PATH` in *main.py* to point to the location of the binary/executable file for phantomjs.
+
+
+## Usage
+`sudo python main.py`
+
+
+## Things to note
+* MacDonald's only loads the first 5 locations. Use selenium to simulate click on *Load More* button to fetch more data.
+* Some websites fetch data asynchronously. Wait for them to finish loading before instantiating a bs4 object.
+
---
+++ main.py
@@ -0,0 +1,31 @@
+import re
+import time
+import urllib2
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+
+PHANTOMJS_PATH = 'bin/phantomjs'
+
+
+def open_url(brower, zipcode):
+  url = 'https://www.mcdonalds.com/us/en-us/restaurant-locator.html?submit=true&radiusSelected=5&searchText={}#rl-listView'.format(zipcode)
+  browser.get(url)
+  time.sleep(20)
+  bs_obj = BeautifulSoup(browser.page_source, 'html.parser')
+  for item in bs_obj.findAll(attrs={'id': re.compile(r'^restaurant-location-list-')}):
+    title = item.find(attrs={'class': 'restaurant-location__title'}).get_text()
+    address = item.find(attrs={'class': 'restaurant-location__address'}).get_text()
+    print '{} {}'.format(title, address)
+
+
+if __name__ == '__main__':
+  # here we'll use pseudo browser PhantomJS,
+  # but browser can be replaced with browser = webdriver.FireFox(),
+  # which is good for debugging.
+  browser = webdriver.PhantomJS(PHANTOMJS_PATH, service_args=['--ignore-ssl-errors=true'])
+  # Do for every zipcode
+  open_url(browser, 94040)
