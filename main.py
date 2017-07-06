427bfae3d3337d652f993e31510a644db23beacf
therealwenqi <therealwenqi@hotmail.com>
2017-07-06 07:15:05 -0700

    cleanup

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import time
 import urllib2
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 
 
 PHANTOMJS_PATH = 'bin/phantomjs'
@@ -23,9 +20,6 @@ def open_url(brower, zipcode):
 
 
 if __name__ == '__main__':
-  # here we'll use pseudo browser PhantomJS,
-  # but browser can be replaced with browser = webdriver.FireFox(),
-  # which is good for debugging.
   browser = webdriver.PhantomJS(PHANTOMJS_PATH, service_args=['--ignore-ssl-errors=true'])
   zipcode = raw_input('What zipcode are you at?\n')
   print('Scraping for data...')
